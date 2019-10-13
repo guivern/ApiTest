8f58712ade80c12cc06222cc8f4b2184f7cf9170
guiver <guiver95@gmail.com>
2019-10-13 13:28:24 -0300

    limpieza de codigo

--- Models/Dtos/CiudadDetailDto.cs
+++ Models/Dtos/CiudadDetailDto.cs
@@ -4,8 +4,8 @@ namespace ApiTest.Models.Dtos
     {
         public long Id { get; set; }
         public string Nombre { get; set; }
-        public bool EsCapital { get; set; }
         public string PaisNombre { get; set; }
         public long IdPais { get; set; }
+        public bool EsCapital { get; set; }
     }
 }
